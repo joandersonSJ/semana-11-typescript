b4f96878687d1de9b12cbd5dea352472428899ed
joandersonSJ <joandersonsantos376@gmail.com>
2020-06-01 04:33:06 -0300

    Home page ended

--- frontend/src/pages/Logon/index.tsx
+++ frontend/src/pages/Logon/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import * as S from './styles';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
 
 const Logon: React.FC = () => (
-  <Container>Logon Page</Container>
+  <>
+    <S.LogonContainer>
+      <S.Form>
+        <img src={logoImg} alt="logo" />
+        <form>
+          <h1>Faça seu Logon</h1>
+          <input placeholder="Sua ID" />
+          <button className="button" type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </a>
+        </form>
+      </S.Form>
+
+      <img src={heroesImg} alt="heroes" />
+    </S.LogonContainer>
+  </>
 );
 
 export default Logon;
--- frontend/src/pages/Logon/styles.ts
+++ frontend/src/pages/Logon/styles.ts
@@ -1,12 +1,45 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  height: 100px;
-  width: 100vw;
-  background-color: #222;
-  color: #fff;
-  font-size: 20px;
-
+export const LogonContainer = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between; 
+
+  width: 100%;
+  max-width: 1120px;
+  height: 100vh;
+  margin: 0 auto;
+`;
+export const Form = styled.section`
+  width: 100%;
+  max-width: 320px;
+  margin-right: 30px;
+    
+    form{
+      margin-top: 100px;
+    } 
+
+    h1{
+      font-size: 32px;
+      margin-bottom: 32px;
+    }
+
+    a{
+      text-decoration: none;
+      color: #41414d;
+      margin-top: 40px;
+      font-size: 18px;
+      font-weight: 500; 
+      transition: filter 0.5s;
+      display: flex;
+      align-items: center;
+      transition: opacity 0.2s;
+    }
+
+    a svg{
+      margin-right: 8px;
+    }
+    a:hover{
+    opacity: 0.8;
+  }
 `;
